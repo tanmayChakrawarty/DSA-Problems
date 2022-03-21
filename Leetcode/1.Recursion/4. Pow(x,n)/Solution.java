e4ef15965c8f8f6865313f9d18cba749e2445529
Tanmay Chakraborty <tanmaychakrabortydhn@outlook.com>
2022-03-21 22:02:45 +0530

    Pow(x,n) - Recursion - Leetcode

---
+++ Leetcode/1.Recursion/4. Pow(x,n)/Solution.java
@@ -0,0 +1,51 @@
+/*
+1.Recusrsion
+*/
+class Solution{
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        double power = myPow(x, n / 2);
+        if (n % 2 == 0) {
+            return power * power;
+        } else {
+            if (n > 0) {
+                return (x * power * power);
+            } else {
+                return (power * power) / x;
+            }
+        }
+    }
+}
+
+/*
+2. Iterative
+*/
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1.0;
+        long nn = n;
+        
+        if (nn < 0) {
+            nn = -1 * nn;
+        }
+        
+        while (nn > 0) {
+            if (nn % 2 == 1) {
+                ans = ans * x;
+                nn = nn - 1;
+            } 
+            else {
+                x = x * x;
+                nn = nn / 2;
+            }
+        }
+        
+        if (n < 0) {
+            ans = (double)(1.0) / (double)(ans);
+        }
+        
+        return ans;
+    }
+}
